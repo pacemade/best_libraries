debb4bebd2ae20be21587c2312b008e8c74fbf13
Danny Chang <dannychachang@gmail.com>
2018-04-09 11:47:17 -0400

    add small checks on user view and layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
+      redirect_to root_url
     else
       flash.now[:alert] = @user.errors.full_messages
       render "new"

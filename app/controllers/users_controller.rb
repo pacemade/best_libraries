fd53fe8db800cea5249c9ad1729afe34ee5372b9
Danny Chang <dannychachang@gmail.com>
2018-04-09 11:31:35 -0400

    create new users view with form, create methods for users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,23 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+    else
+      flash.now[:alert] = @user.errors.full_messages
+      render "new"
+    end
+  end
+
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+
 end

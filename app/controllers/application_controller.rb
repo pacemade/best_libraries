1e9dab8a69997ed58f2ace6517284df25529b5ee
Danny Chang <dannychachang@gmail.com>
2018-04-09 11:44:51 -0400

    add current user and logged in method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+
+  def require_login
+    unless current_user
+      flash[:alert] = "You must be logged in to see this person's contact information!"
+      redirect_to login_path
+    end
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_user
 end

9c229b23a788f380a18796b3ed98e88c164013b6
Danny Chang <dannychachang@gmail.com>
2018-04-09 12:09:27 -0400

    create new sessions view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
+      redirect_to users_path
     else
       flash.now[:notice] = "Invalid email or password"
       render "new"

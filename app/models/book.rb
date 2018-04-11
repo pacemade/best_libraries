003fb46eb22964ceb7f561f2d7b723bffb823be6
Danny Chang <dannychachang@gmail.com>
2018-04-11 18:35:10 -0400

    move nav links around

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,8 @@
 class Book < ApplicationRecord
   require "csv"
 
+  validates :title, :author, :genre, :subgenre, :pages, :publisher, :copies, presence: true
+
   has_many :borrows
   has_many :users, through: :borrows, source: :user
 

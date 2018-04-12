b2729d9696cefcf27e4aee745d11ea1367d6f676
Danny Chang <dannychachang@gmail.com>
2018-04-12 10:47:19 -0400

    remove validations on books that stopped import for pt1

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ApplicationRecord
   require "csv"
 
-  validates :title, :author, :genre, :subgenre, :pages, :publisher, :copies, presence: true
+  validates :title, :pages, :copies, presence: true
 
   has_many :borrows
   has_many :users, through: :borrows, source: :user
